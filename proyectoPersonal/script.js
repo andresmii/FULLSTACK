97f1cddffbd589d829a9d90ca7b890ef6fe295af
andresmii <andresmleon@protonmail.com>
2023-03-08 11:53:58 +0100

    update on responsive for products and index pages

--- proyectoPersonal/script.js
+++ proyectoPersonal/script.js
@@ -9,3 +9,29 @@ function myFunction() {
     burgerMenu.style.display = "block";
   }
 }
+
+let d = 20;
+let r = d / 2;
+let area = Math.round(Math.PI * Math.pow(r, 2));
+console.log(area);
+
+let random = Math.random() + 2;
+console.log(random);
+
+let dice = Math.trunc(Math.random() * 6 + 1);
+console.log(dice);
+
+let x = 120;
+let y = 85;
+let z = 24;
+let sum = x + y + z;
+let discount = 0.4;
+
+let price = function (cost) {
+  return cost * (1 - discount);
+};
+console.log(
+  `the full price of the objects is ${sum}€, with a ${
+    discount * 100
+  }% discount the final price will be ${price(sum)}€`
+);
