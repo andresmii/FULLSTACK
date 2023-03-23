89001fc274af05c2b102799d63c405e2d4c3d435
andresmii <andresmleon@protonmail.com>
2023-03-23 11:21:04 +0100

    fixed the script tag and creating new files for the lessons, starting with .json files

--- proyectoPersonal/clases.js
+++ proyectoPersonal/clases.js
@@ -1,117 +1,3 @@
-"use strict";
-
-//////////////////////////////////////////////////
-//////////////////////////////////////////////////
-//////////////////////////////////////////////////
-///////////// CREATE-DISPLAY PRODUCT /////////////
-
-const productContainer = document.querySelector(`.product-wrapper`);
-// const productPageContainer = document.querySelector(`.product-layout`);
-
-function Product(cat, color, desc, cost, img, price, prod, stock) {
-  this.category = cat;
-  this.color = color;
-  this.description = desc;
-  this.cost = cost;
-  this.image = img;
-  this.price = price;
-  this.product = prod;
-  this.stock = stock;
-}
-
-const product1 = new Product(
-  `t shirt`,
-  `black`,
-  `a cool black oversized t shirt`,
-  10,
-  `194942208706-1_1280x.png`,
-  20,
-  `oversized t shirt`,
-  50
-);
-
-const product2 = new Product(
-  `sweatshirt`,
-  [`navy`, `black`, `pearl white`, `yellow`, `gray`],
-  `the comfiest hoodie you can have`,
-  23,
-  `194942208706-2_1280x.png`,
-  40,
-  `oversized hoodie`,
-  20
-);
-
-const product3 = new Product(
-  `sweatshirt`,
-  [`black`, `white`, `gray`, `burgundy`],
-  `i can't promise your gf is not going to steal you this one`,
-  20,
-  `th-198647226.png`,
-  30,
-  `oversized sweatshirt`,
-  20
-);
-
-const product4 = new Product(
-  `pants`,
-  [`navy`, `black`, `grey`],
-  `some cool pants made by us with much love for you`,
-  30,
-  `th-750742328.png`,
-  45,
-  `cool loose pants`,
-  10
-);
-
-const product5 = new Product(
-  `shorts`,
-  [`navy`, `black`, `red`, `orange`],
-  `they can also be used as a swimsuit`,
-  10,
-  `th-1029833788.png`,
-  18,
-  `comfortable shorts`,
-  60
-);
-
-const inventory = [];
-inventory.push(product1, product2, product3, product4, product5);
-
-// function for displaying the products on the array on the index.html page
-const displayProduct = () => {
-  productContainer.innerHTML = ``;
-  // productPageContainer.innerHTML = ``;
-
-  for (let i = 0; i < inventory.length; i++) {
-    const html = `
-    <div class="product">
-      <div class="product-display">
-        <a href="">
-          <img
-          src="img/product_image/${inventory[i].image}"
-          alt="${inventory[i].category}"
-          />
-        </a>
-        <a href="#"><h4>${inventory[i].product}</h4></a>
-        <h5>${inventory[i].description}</h5>
-        <p>${inventory[i].price} €</p>
-      </div>
-    </div>
-    `;
-
-    productContainer.insertAdjacentHTML(`beforeend`, html);
-    // productPageContainer.insertAdjacentHTML(`beforeend`, html);
-  }
-};
-displayProduct();
-
-//////////////////////////////////////////////////
-//////////////////////////////////////////////////
-//////////////////////////////////////////////////
-//////////////////////////////////////////////////
-//////////////////////////////////////////////////
-//////////////////////////////////////////////////
-
 // const prod = {
 //   prodName: `Alpargatas NIKE`,
 //   price: 80,
@@ -1020,30 +906,30 @@ displayProduct();
 /////////////////////////
 /////////////////////////
 
-// // fetching API
-// const getEmployees = () => {
-//   fetch(`data.json`)
-//     .then((res) => {
-//       return res.json();
-//     })
-//     .then((data) => {
-//       console.log(data);
-//       const { employees } = data;
-//       employees.forEach((emp) => {
-//         console.log(`the employee ${emp.name} works as ${emp.position}`);
-//       }); // use destructuring to get a specific property
-//     });
-// };
-// getEmployees();
+// fetching API
+const getEmployees = () => {
+  fetch(`data.json`)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      const { employees } = data;
+      employees.forEach((emp) => {
+        console.log(`the employee ${emp.name} works as ${emp.position}`);
+      }); // use destructuring to get a specific property
+    });
+};
+getEmployees();
 
-// async function getEmployees2() {
-//   const res = await fetch(`data.json`);
-//   const data = await res.json();
-//   console.log(data);
+async function getEmployees2() {
+  const res = await fetch(`data.json`);
+  const data = await res.json();
+  console.log(data);
 
-//   const { employees } = data;
-//   employees.forEach((emp) => {
-//     console.log(`the employee ${emp.name} works as ${emp.position}`);
-//   });
-// }
-// getEmployees2();
+  const { employees } = data;
+  employees.forEach((emp) => {
+    console.log(`the employee ${emp.name} works as ${emp.position}`);
+  });
+}
+getEmployees2();
